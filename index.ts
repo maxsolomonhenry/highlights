312f01f3b6bb3f97010ad3cc1b5aedb99f506f7c
Max Henry <mhenry@eers.ca>
2026-03-11 21:51:44 -0400

    poc sends pdf and gets text

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
+import fs from "fs";
 import 'dotenv/config';
+import OpenAI from "openai";
+import { pdf } from "pdf-to-img";
+
+const openai = new OpenAI({
+  baseURL: 'https://api.deepinfra.com/v1/openai',
+  apiKey: process.env.DEEPINFRA_API_KEY,
+});
+
+async function main() {
+  const pages = await pdf("fixtures/Gebgreegziabher 2025 - SupportingCoadaptiveMachineTeaching.pdf");
+  
+  for await (const page of pages) {
+    const base64Image = page.toString('base64');
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_url",
+              image_url: {
+                "url": `data:image/png;base64,${base64Image}`
+              }
+            }
+          ]
+        }
+      ],
+      model: "allenai/olmOCR-2-7B-1025",
+      max_tokens: 4092,
+    });
+    console.log(completion.choices[0]?.message.content);
+    // console.log(completion.usage?.prompt_tokens, completion.usage?.completion_tokens);
+    break;
+  }
+
+}
+
+main();
